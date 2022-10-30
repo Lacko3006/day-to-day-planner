44936bd0f07c614c8be2e575848116ba78b9bf87
Sam Laxton <samlaxton@sams-air-2.home>
2022-10-30 13:23:20 +0000

    added if statement to be completed

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,21 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
+// function comparing the day planner to current time.
+function TimeTracker() {
+  let hourOfDay = moment().hour();
+  $(".time-block").each(function () {
+    let currentHour = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(currentHour)
+    if (currentHour < hourOfDay) {
+    }
+  });
+
+}
+
+//click event listener to add to local storage
 $("#container").on("click", "button", function () {
   let text = $(this).siblings(".description").val();
   let time = $(this).parent().attr("id");
   localStorage.setItem(time, text);
-  console.log("hello");
 });
 
-// create time of day for each time-block
-function TimeTracker() {
-  let hourOfDay = moment().hour();
-  
-  $(".time-block").each(function () {
-    let currentHour = parseInt($(this).attr("id").split("hour")[1]);
-    console.log(currentHour.parseInt);
-  });
-
-
-console.log(hourOfDay)
-
-}
-
-TimeTracker()
