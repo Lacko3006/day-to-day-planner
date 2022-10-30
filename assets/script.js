807510b5db376d22569e1508ba69a7305dffd6b1
Sam Laxton <samlaxton@sams-air-2.home>
2022-10-30 12:08:36 +0000

    added new event listener'

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 moment().format();
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-$("#saveBtn").on("click", function(){
-    console.log(this)
-})
+$('#container'). on( 'click', 'button', function() {
+    let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+        console.log("hello")
+        });
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
