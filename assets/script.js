d3a774bcacf71e2d6aba8bef781660c8dafff8b6
Sam Laxton <samlaxton@sams-air-2.home>
2022-10-30 13:03:06 +0000

    added id to div instead of H4s

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,24 @@
-moment().format();
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-$('#container'). on( 'click', 'button', function() {
-    let text = $(this).siblings(".description").val();
-        let time = $(this).parent().attr("id");
-        localStorage.setItem(time, text);
-        console.log("hello")
-        });
+$("#container").on("click", "button", function () {
+  let text = $(this).siblings(".description").val();
+  let time = $(this).parent().attr("id");
+  localStorage.setItem(time, text);
+  console.log("hello");
+});
 
-// create current time for each time-block
+// create time of day for each time-block
+function TimeTracker() {
+  let hourOfDay = moment().hour();
+  
+  $(".time-block").each(function () {
+    let currentHour = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(currentHour.parseInt);
+  });
+
+
+console.log(hourOfDay)
+
+}
+
+TimeTracker()
